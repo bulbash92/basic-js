d800400eedb31af4d3ddcf678ca0df88c488115b
Evgeniy <49danon49@gmail.com>
2020-02-21 21:50:56 +0300

    feat: solve task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) { return false; }
+  const commandName = [];
+  for (const name of members) {
+    if (typeof name == 'string') {
+      commandName.push((name.match(/[a-z]/i, '')[0]).toUpperCase());
+    }
+  }
+
+  return commandName.length > 0 ? commandName.sort().join('') : false;
 };
