cf7dd5612c0c658dbb3868bdf5ef38bcd56cefb2
Evgeniy <49danon49@gmail.com>
2020-03-09 22:28:04 +0300

    feat: add finish task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw 'Error';
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            arr.splice(arr[i + 1], 1);
+        } else if (arr[i] === '--discard-prev') {
+            if (i === arr.length-1) {
+                arr.splice(arr[i], 1);
+            } else {
+            arr.splice(arr[i - 1], 1);
+        }
+        } else if (arr[i] === '--double-next') {
+            arr.splice(arr[i + 1], 0, arr[i]);
+        } else if (arr[i] === '--double-prev') {
+            arr.splice(arr[i - 1], 0, arr[i]);
+        } else if (arr.length < 1) {
+            return arr;
+        }
+        
+    } return arr;
 };
